a38f10cb957696b9c33fb5d253baf9301145f4fb
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-07-02 21:30:37 +0800

    feat: better menu

--- pages/callback.vue
+++ pages/callback.vue
@@ -8,9 +8,15 @@
     <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       <div v-for="guild in sortedGuilds" :key="guild.id"
         :class="{ 'bg-green-500': guild.has_bot, 'bg-gray-700': !guild.has_bot }"
-        class="p-4 rounded-lg shadow-lg text-white flex flex-col items-center justify-center">
-        <h2 class="text-xl font-bold">{{ guild.name }}</h2>
-        <p class="text-sm">ID: {{ guild.id }}</p>
+        class="p-4 rounded-lg shadow-lg text-white flex items-center space-x-4 transform transition-transform duration-300 hover:scale-110"
+      >
+        <img :src="getIconUrl(guild)" alt="Server Icon" class="w-12 h-12 rounded-full">
+        <div>
+          <h2 class="text-xl font-bold">{{ guild.name }}</h2>
+          <p class="text-sm">ID: {{ guild.id }}</p>
+          <button v-if="guild.has_bot" class="mt-2 bg-gray-800 text-white py-1 px-2 rounded hover:bg-gray-700 transition">Manage</button>
+          <button v-else class="mt-2 bg-blue-500 text-white py-1 px-2 rounded hover:bg-blue-400 transition">Invite to server</button>
+        </div>
       </div>
     </div>
   </div>
@@ -35,6 +41,9 @@ const hasManageServerPermission = (guild) => {
 const sortedGuilds = computed(() => {
   return guilds.value ? guilds.value.slice().sort((a, b) => b.has_bot - a.has_bot) : []
 })
+
+const defaultIconUrl = 'https://cdn.discordapp.com/embed/avatars/0.png'
+const getIconUrl = (guild) => !guild.icon ? defaultIconUrl : `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`
 
 onMounted(async () => {
   try {
