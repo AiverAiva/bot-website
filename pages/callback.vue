872c252f8d27627df03393d6b1f24b58793adb7b
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-07-02 17:34:14 +0800

    feat: permission filter

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,29 +1,19 @@
 <template>
   <div class="min-h-screen flex flex-col items-center justify-center bg-gray-900 p-4">
     <div class="pt-16"></div>
-      <!-- <div class="container ">
-        <h1 class="text-3xl font-bold mt-8">Welcome to Your Website</h1>
-        <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-      </div> -->
-
-      <div class="bg-white p-8 rounded mx-auto shadow-md w-full max-w-md mb-8">
-        <h1 class="text-2xl font-bold text-center">Callback</h1>
-        <!-- <div v-if="user" class="mb-4">
-        <img :src="user.avatar_url" alt="User Avatar" class="rounded-full w-24 h-24 mb-4 transition-opacity duration-300 opacity-75 hover:opacity-100">
-        <p>Logged in as: {{ user.username }}#{{ user.discriminator }}</p>
-      </div> -->
-        <!-- <button class="p-2 rounded hover:opacity-100 transition-opacity duration-300 opacity-75">Button</button> -->
+    <div class="bg-white p-8 rounded mx-auto shadow-md w-full max-w-md mb-8">
+      <h1 class="text-2xl font-bold text-center">Callback</h1>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div v-for="guild in sortedGuilds" :key="guild.id"
+        :class="{ 'bg-green-500': guild.has_bot, 'bg-gray-700': !guild.has_bot }"
+        class="p-4 rounded-lg shadow-lg text-white flex flex-col items-center justify-center">
+        <h2 class="text-xl font-bold">{{ guild.name }}</h2>
+        <p class="text-sm">ID: {{ guild.id }}</p>
       </div>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <div v-for="guild in sortedGuilds" :key="guild.id"
-          :class="{ 'bg-green-500': guild.has_bot, 'bg-gray-700': !guild.has_bot }"
-          class="p-4 rounded-lg shadow-lg text-white flex flex-col items-center justify-center">
-          <h2 class="text-xl font-bold">{{ guild.name }}</h2>
-          <p class="text-sm">ID: {{ guild.id }}</p>
-        </div>
-      </div>
+    </div>
 
-    
+
   </div>
 </template>
 
@@ -34,24 +24,27 @@ import { useMainStore } from '~/stores/index'
 const store = useMainStore()
 const config = useRuntimeConfig()
 
-const user = computed(() => store.user)
+// const user = computed(() => store.user)
 const guilds = computed(() => store.guilds)
-// const config = useRuntimeConfig()
-// const user = ref(null)
-// const guilds = ref([])
 
 onMounted(async () => {
   const code = new URLSearchParams(window.location.search).get('code')
   const { data } = await axios.get(`${config.public.API_URL}/callback?code=${code}`)
   const accessToken = data.accessToken
   const Response = await axios.get(`${config.public.API_URL}/user?accessToken=${accessToken}`)
-  user.value = Response.data.user
-  guilds.value = Response.data.user_guilds
+  // user.value = Response.data.user
+  // guilds.value = Response.data.user_guilds
 
   console.log(Response.data.user)
   store.setUser(Response.data.user)
-  store.setGuilds(Response.data.user_guilds)
+  store.setGuilds(Response.data.user_guilds.filter(guild => hasManageServerPermission(guild)))
 })
+
+const hasManageServerPermission = (guild) => {
+  const permissions = BigInt(guild.permissions)
+  const MANAGE_SERVER = BigInt(0x20)
+  return (permissions & MANAGE_SERVER) === MANAGE_SERVER
+}
 
 const sortedGuilds = computed(() => {
   return guilds.value ? guilds.value.slice().sort((a, b) => b.has_bot - a.has_bot) : []
