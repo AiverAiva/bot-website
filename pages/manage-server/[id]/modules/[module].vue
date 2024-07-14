da9b8a4195c4c4cdc2fd8faf7a1faed4a96ed9e8
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-07-14 15:56:52 +0800

    revamp: nvm idc

--- pages/manage-server/[id]/modules/[module].vue
+++ pages/manage-server/[id]/modules/[module].vue
@@ -155,7 +155,7 @@ const fetchRoles = async () => {
 
 const addRole = () => {
     if (newLevel.value && selectedRole.value) {
-        module.value.settings.LEVEL_ROLES[Number(newLevel.value)] = selectedRole.value
+        module.value.settings.LEVEL_ROLES[newLevel.value] = selectedRole.value
         levelInputs.value[newLevel.value] = newLevel.value
         newLevel.value = ''
         selectedRole.value = ''
